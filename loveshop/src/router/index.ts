a138ad85c6d53709f9f036b2b19b157cdb4374e3
RomanEger <egerromaan@gmail.com>
2026-01-28 16:26:55 +0600

    rename views

--- loveshop/src/router/index.ts
+++ loveshop/src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
-import Register from '../views/Register.vue'
+import Home from '../views/HomeItem.vue'
+import Login from '../views/LoginItem.vue'
+import Register from '../views/RegisterItem.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
